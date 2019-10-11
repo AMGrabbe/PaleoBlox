b22eec4c52aa1ec604dbde4979a9434f59b0696e
Anne-Marie Grabbe <56431940+AMGrabbe@users.noreply.github.com>
2019-10-11 11:31:05 +0200

    Initial Commit

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    [SerializeField] Paddle paddleOne;
+    Vector2 distance;
+    bool hasStarted = false;
+    [SerializeField] float velocityX = 3f;
+    [SerializeField] float velocityY = 15f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        distance = transform.position - paddleOne.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!hasStarted)
+            StickBallToPaddle();
+        LaunchBallOnMouseClick();
+    }
+
+    void StickBallToPaddle()
+    {
+        Vector2 paddleOnePosition = new Vector2(paddleOne.transform.position.x, paddleOne.transform.position.y);
+        transform.position = paddleOnePosition + distance;
+    }
+
+    void LaunchBallOnMouseClick()
+    {
+        if (Input.GetMouseButtonDown(0) && !hasStarted)
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(velocityX, velocityY);
+            hasStarted = true;
+        }
+    }
+}
---
+++ Assets/Scripts/Paddle.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour
+{
+    [SerializeField] float widthInUnityUnits = 16f;
+    [SerializeField] float ScreenBoundaryMin = 1f;
+    [SerializeField] float ScreenBoundaryMax = 15f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mouseXPositionInUnits = Input.mousePosition.x / Screen.width * widthInUnityUnits;
+        Vector2 paddlePosition = new Vector2(mouseXPositionInUnits, transform.position.y);
+        paddlePosition.x = Mathf.Clamp( mouseXPositionInUnits, ScreenBoundaryMin, ScreenBoundaryMax);
+        transform.position = paddlePosition;
+    }
+}
