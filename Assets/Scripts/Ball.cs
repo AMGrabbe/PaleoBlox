ef99f035a6c0f58e8136c3c9d8933b1b842ad165
Anne-Marie Grabbe <56431940+AMGrabbe@users.noreply.github.com>
2019-10-25 05:49:20 +0200

    SoundsImplemented
    Different sounds when ball collides with objects.

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -5,37 +5,56 @@ using UnityEngine;
 public class Ball : MonoBehaviour
 {
     [SerializeField] Paddle paddleOne;
+    [SerializeField] float velocityX = 2f;
+    [SerializeField] float velocityY = 15f;
+    [SerializeField] AudioClip[] ballSound;
+
+
     Vector2 distance;
     bool hasStarted = false;
-    [SerializeField] float velocityX = 3f;
-    [SerializeField] float velocityY = 15f;
+    AudioSource myAudioSource;
+
 
     // Start is called before the first frame update
     void Start()
     {
         distance = transform.position - paddleOne.transform.position;
+        myAudioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
     void Update()
     {
         if (!hasStarted)
+        {
             StickBallToPaddle();
-        LaunchBallOnMouseClick();
+
+            LaunchBallOnMouseClick();
+        }
     }
 
-    void StickBallToPaddle()
+    private void StickBallToPaddle()
     {
         Vector2 paddleOnePosition = new Vector2(paddleOne.transform.position.x, paddleOne.transform.position.y);
         transform.position = paddleOnePosition + distance;
     }
 
-    void LaunchBallOnMouseClick()
+    private void LaunchBallOnMouseClick()
     {
-        if (Input.GetMouseButtonDown(0) && !hasStarted)
+        if (Input.GetMouseButtonDown(0))
         {
-            GetComponent<Rigidbody2D>().velocity = new Vector2(velocityX, velocityY);
             hasStarted = true;
+            GetComponent<Rigidbody2D>().velocity = new Vector2(velocityX, velocityY);
         }
+    }
+    
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        AudioClip clip = ballSound[UnityEngine.Random.Range(0, ballSound.Length)];
+        if (hasStarted)
+        {
+            myAudioSource.PlayOneShot(clip);
+        }
+
     }
 }
